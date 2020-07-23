01e65a046f91e766678d8420a8fef51904c63083
denismoroz1981 <denismoroz@mail.ru>
2020-07-23 10:54:04 +0300

    heroku_change dir to static

--- fintransfer/settings.py
+++ fintransfer/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = '/cabinet/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATICFILES_DIRS = (
-  os.path.join(BASE_DIR, '/static/'),
+  os.path.join(BASE_DIR, 'cabinet/static/'),
 )
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
